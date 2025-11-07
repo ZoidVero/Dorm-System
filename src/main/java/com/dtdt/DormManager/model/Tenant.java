03c0d76cadd033eed0c9925a47df083bcde236b2
cons <119858107+Enitnat@users.noreply.github.com>
2025-11-07 08:19:20 +0800

    Update Tenant.java

--- src/main/java/com/dtdt/DormManager/model/Tenant.java
+++ src/main/java/com/dtdt/DormManager/model/Tenant.java
@@ -2,18 +2,16 @@ package com.dtdt.DormManager.model;
 
 public class Tenant extends User {
 
-    // Tenant-specific properties
+    
     private int currentYear;
     private String assignedRoomID;
 
     // The constructor must call the parent 'User' constructor using 'super()'
     public Tenant(String userId, String email, String password, String fullName, int currentYear) {
-        // 'super' passes these values up to the User class constructor
         super(userId, email, password, fullName);
         this.currentYear = currentYear;
     }
 
-    // Getters and Setters for tenant-specific properties
     public int getCurrentYear() { return currentYear; }
     public void setCurrentYear(int currentYear) { this.currentYear = currentYear; }
 
